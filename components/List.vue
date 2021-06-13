e6e48b5bc649b59d506d1653ba51df0e8a63c6ac
ElvisPoe <elvis.poe96@yahoo.com>
2021-06-14 00:04:33 +0300

    GetJson Data Dev

---
+++ components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Cocktails</h1>
+    <p v-for="cocktail of cocktails">{{ cocktail }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cocktails: [],
+    }
+  },
+  async fetch() {
+    this.cocktails = await fetch(
+      'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+    ).then((res) => res.json())
+
+    this.cocktailsByType = await fetch(
+      'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin'
+    ).then((res) => res.json())
+
+    ///https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=15300
+  },
+}
+</script>
---
+++ components/Search.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="centerx icons-example">
+<!--    <vs-input class='search-bar' placeholder="Search Cocktail" />-->
+  </div>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuesaxLogo from '~/components/VuesaxLogo.vue'
+//
+// import Search from '~/components/Search.vue'
+// import List from '~/components/List.vue'
+//
+// export default {
+//   components: {
+//     Logo,
+//     VuesaxLogo,
+//     Search,
+//     List
+//   }
+// }
+</script>
+
+<style scoped>
+.search-bar input {
+  background: red;
+  width: 50%;
+  margin: auto;
+}
+
+</style>
